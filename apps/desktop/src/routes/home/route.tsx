999ca5547b8648b77412d69840d1da5a622dbbc0
thiratt <thiratcha7@gmail.com>
2025-11-26 18:08:21 +0700

    feat(desktop): integrate NotificationSidebar and enhance DesktopTitlebar with helper actions

--- apps/desktop/src/routes/home/route.tsx
+++ apps/desktop/src/routes/home/route.tsx
@@ -8,8 +8,13 @@ import {
 } from "@tanstack/react-router";
 
 import { HomeSidebar } from "@workspace/app-ui/components/home-sidebar";
+import { NotificationSidebar } from "@workspace/app-ui/components/notification-sidebar";
+import { useNekoShare } from "@workspace/app-ui/context/nekoshare";
+import { useMemo } from "react";
+import { LuBell, LuSettings } from "react-icons/lu";
 
 export const Route = createFileRoute("/home")({
+  // TODO: Do not forget to enable authentication
   // async beforeLoad() {
   //   const { data: session, error } = await authClient.getSession();
   //   const isAuthenticated = !!session?.user && !error;
@@ -21,9 +26,28 @@ export const Route = createFileRoute("/home")({
 });
 
 function RouteComponent() {
+  const { notification, toggleNotification } = useNekoShare();
+
+  const titlebarHelperActions = useMemo(
+    () => [
+      {
+        icon: <LuBell />,
+        onClick: () => toggleNotification(),
+        badge: true,
+        actived: notification === "on",
+      },
+      {
+        icon: <LuSettings />,
+        onClick: () => {
+          console.log("Settings clicked");
+        },
+      },
+    ],
+    [toggleNotification],
+  );
   return (
     <div className="min-h-svh flex flex-col">
-      <DesktopTitlebar />
+      <DesktopTitlebar helperActions={titlebarHelperActions} />
       <div className="flex flex-1 divide-x">
         <HomeSidebar
           linkComponent={Link}
@@ -33,9 +57,7 @@ function RouteComponent() {
         <div className="flex-1 bg-muted p-4">
           <Outlet />
         </div>
-        {/* <AnimatePresence mode="wait">
-          {notification === "on" && <NotificationSidebar />}
-        </AnimatePresence> */}
+        <NotificationSidebar />
       </div>
     </div>
   );
