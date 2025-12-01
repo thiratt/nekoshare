aee131eaec5a5f41520e3772386349e19ccad8aa
thiratt <thiratcha7@gmail.com>
2025-12-01 16:37:48 +0700

    feat(app-ui): add better-auth dependency and implement authClient setup

---
+++ packages/app-ui/src/lib/auth.ts
@@ -0,0 +1,19 @@
+import { createAuthClient } from "better-auth/react";
+// import { inferAdditionalFields } from "better-auth/client/plugins";
+
+export const authClient: ReturnType<typeof createAuthClient> = createAuthClient({
+	baseURL: "http://localhost:7780",
+	basePath: "/auth",
+});
+
+// export const {
+//   useSession,
+//   signIn,
+//   signUp,
+//   signOut,
+//   forgetPassword,
+//   resetPassword,
+// } = authClient;
+
+// export type Session = typeof authClient.$Infer.Session;
+// export type User = typeof authClient.$Infer.Session.user;
