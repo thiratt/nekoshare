2afa8460fd05ad6e54342df401919b3425e88168
thiratt <thiratcha7@gmail.com>
2025-12-01 17:05:32 +0700

    feat(desktop): implement login flow

--- apps/desktop/src/routes/(auth)/login.tsx
+++ apps/desktop/src/routes/(auth)/login.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute, Link, useRouter } from "@tanstack/react-router";
 
 import { LoginCard } from "@workspace/app-ui/components/login-card";
+import { authClient } from "@workspace/app-ui/lib/auth";
 import type { TLoginSchema } from "@workspace/app-ui/types/schema";
 export const Route = createFileRoute("/(auth)/login")({
   component: RouteComponent,
@@ -18,19 +19,31 @@ function RouteComponent() {
   };
 
   const onSubmit = async (data: TLoginSchema) => {
-    const res = await fetch("http://localhost:9988/auth/login", {
-      method: "POST",
-      body: JSON.stringify(data),
+    const { error } = await authClient.signIn.email({
+      email: data.identifier,
+      password: data.password,
     });
 
-    if (res.ok) {
-      // const store = await load("store.json");
-      // const account = await res.json();
-
-      // await store.set("account", account);
-
-      router.navigate({ to: "/home" });
+    if (error) {
+      console.error("Login error:", error);
+      alert(`Login failed: ${error.message}`);
+      return;
     }
+
+    router.navigate({ to: "/home", replace: true });
+    // const res = await fetch("http://localhost:9988/auth/login", {
+    //   method: "POST",
+    //   body: JSON.stringify(data),
+    // });
+
+    // if (res.ok) {
+    //   // const store = await load("store.json");
+    //   // const account = await res.json();
+
+    //   // await store.set("account", account);
+
+    //   router.navigate({ to: "/home" });
+    // }
   };
 
   return (
