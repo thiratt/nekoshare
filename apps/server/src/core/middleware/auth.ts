3e15650752ade9bb5da3ee2074e34eaed219da9a
thiratt <thiratcha7@gmail.com>
2025-12-02 13:24:01 +0700

    fix(server): streamline unauthorized response handling in auth middleware

--- apps/server/src/core/middleware/auth.ts
+++ apps/server/src/core/middleware/auth.ts
@@ -1,23 +1,11 @@
-import { Hono } from "hono";
-import { serve } from "@hono/node-server";
-import { cors } from "hono/cors";
 import { auth } from "@/core/auth";
 import { createMiddleware } from "hono/factory";
-
-// const authMiddleWare = new Hono<{
-// 	Variables: {
-// 		user: typeof auth.$Infer.Session.user | null;
-// 		session: typeof auth.$Infer.Session.session | null;
-// 	};
-// }>();
 
 const authMiddleWare = createMiddleware(async (c, next) => {
 	const session = await auth.api.getSession({ headers: c.req.raw.headers });
 	c.header("Server", "NekoShare");
 	if (!session) {
-		c.set("user", null);
-		c.set("session", "null");
-		return next();
+		return c.json({ error: "Unauthorized" }, 401);
 	}
 
 	c.set("user", session.user);
