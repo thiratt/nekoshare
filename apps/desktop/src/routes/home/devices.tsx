a9166b987264cf27bd252008e06eac1215b8f266
thiratt <thiratcha7@gmail.com>
2025-12-02 18:19:56 +0700

    fix(desktop): update type for local device info and adjust API invocation

--- apps/desktop/src/routes/home/devices.tsx
+++ apps/desktop/src/routes/home/devices.tsx
@@ -4,20 +4,22 @@ import { createFileRoute } from "@tanstack/react-router";
 import { invoke } from "@tauri-apps/api/core";
 
 import { DevicesUI } from "@workspace/app-ui/components/ui/devices";
-import type { DeviceInfo } from "@workspace/app-ui/types/device";
+import type { LocalDeviceInfo } from "@workspace/app-ui/types/device";
 
 export const Route = createFileRoute("/home/devices")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const [localDeviceInfo, setDeviceInfo] = useState<DeviceInfo | null>(null);
+  const [localDeviceInfo, setDeviceInfo] = useState<LocalDeviceInfo | null>(
+    null,
+  );
 
   useEffect(() => {
-    invoke("get_device_info").then((info) => {
-      setDeviceInfo(info as DeviceInfo);
+    invoke<LocalDeviceInfo>("get_device_info").then((info) => {
+      setDeviceInfo(info);
     });
   }, []);
 
-  return <DevicesUI deviceInfo={localDeviceInfo} />;
+  return <DevicesUI localDeviceInfo={localDeviceInfo} />;
 }
