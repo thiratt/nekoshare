3631418d9fd7aa7edc3c5679e5b72a2962fde765
thiratt <thiratcha7@gmail.com>
2025-12-07 07:46:17 +0700

    fix(app-ui): update grid layout for device cards in DevicesUI component

--- packages/app-ui/src/components/ui/devices.tsx
+++ packages/app-ui/src/components/ui/devices.tsx
@@ -397,7 +397,7 @@ export function DevicesUI({ localDeviceInfo }: DevicesUIProps) {
 
 				{hasDevices ? (
 					<ScrollArea className="h-[calc(100vh-14rem)]">
-						<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+						<div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-2">
 							{filteredDevices.map((device) => (
 								<DeviceCard
 									key={device.id}
