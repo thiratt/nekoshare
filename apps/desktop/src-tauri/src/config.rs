22d6899434bce4181688390bd241fbd5e412811c
thiratt <thiratcha7@gmail.com>
2025-12-22 07:16:04 +0700

    feat(config): add configuration for API and socket server endpoints

---
+++ apps/desktop/src-tauri/src/config.rs
@@ -0,0 +1,12 @@
+#[cfg(debug_assertions)]
+pub mod constants {
+    pub const API_SERVER_ENDPOINT: &str = "http://localhost:7780";
+    pub const SOCKET_SERVER_ENDPOINT: &str = "127.0.0.1";
+    pub const SOCKET_SERVER_PORT: u16 = 7781;
+}
+
+#[cfg(not(debug_assertions))]
+pub mod constants {
+    pub const API_SERVER_ENDPOINT: &str = "replace_with_production_endpoint";
+    pub const SOCKET_SERVER_ENDPOINT: &str = "replace_with_production_endpoint";
+}
