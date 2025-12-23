71c8aad5b0b65d6a85f8aca3816074c3266c0f43
thiratt <thiratcha7@gmail.com>
2025-12-24 06:41:13 +0700

    feat(eslint): add simple-import-sort plugin and update ESLint settings

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -4,29 +4,54 @@ import turboPlugin from "eslint-plugin-turbo";
 import tseslint from "typescript-eslint";
 import onlyWarn from "eslint-plugin-only-warn";
 
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+
 /**
  * A shared ESLint configuration for the repository.
  *
  * @type {import("eslint").Linter.Config[]}
  * */
 export const config = [
-  js.configs.recommended,
-  eslintConfigPrettier,
-  ...tseslint.configs.recommended,
-  {
-    plugins: {
-      turbo: turboPlugin,
-    },
-    rules: {
-      "turbo/no-undeclared-env-vars": "warn",
-    },
-  },
-  {
-    plugins: {
-      onlyWarn,
-    },
-  },
-  {
-    ignores: ["dist/**"],
-  },
+	js.configs.recommended,
+	eslintConfigPrettier,
+	...tseslint.configs.recommended,
+	{
+		plugins: {
+			turbo: turboPlugin,
+			"simple-import-sort": simpleImportSort,
+		},
+		rules: {
+			"turbo/no-undeclared-env-vars": "warn",
+			"simple-import-sort/imports": [
+				"error",
+				{
+					groups: [
+						// Node.js built-ins
+						["^node:"],
+
+						// External packages
+						["^@?\\w"],
+
+						// Internal packages (workspace aliases)
+						["^@workspace/"],
+
+						// Relative imports
+						["^\\./", "^\\.\\./"],
+
+						// Side-effect imports
+						["^\\u0000"],
+					],
+				},
+			],
+			"simple-import-sort/exports": "error",
+		},
+	},
+	{
+		plugins: {
+			onlyWarn,
+		},
+	},
+	{
+		ignores: ["dist/**"],
+	},
 ];
