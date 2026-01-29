fc5b9cbcdb41ea432be3ad4a33dea5d673ff3840
thiratt <thiratcha7@gmail.com>
2026-01-29 11:55:53 +0700

    refactor(desktop): rename SOCKET_SERVER_PORT to SOCKET_SERVER_ENDPOINT

--- apps/desktop/src-tauri/src/config.rs
+++ apps/desktop/src-tauri/src/config.rs
@@ -1,8 +1,7 @@
 #[cfg(debug_assertions)]
 pub mod constants {
     pub const API_SERVER_ENDPOINT: &str = "http://localhost:7780";
-    pub const SOCKET_SERVER_ENDPOINT: &str = "127.0.0.1";
-    pub const SOCKET_SERVER_PORT: u16 = 7781;
+    pub const SOCKET_SERVER_ENDPOINT: &str = "127.0.0.1:7781";
 }
 
 #[cfg(not(debug_assertions))]
