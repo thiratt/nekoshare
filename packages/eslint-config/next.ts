a3fb53bf6952ec1f03c1e1ab3b28cbac85816b26
thiratt <thiratcha7@gmail.com>
2026-03-11 14:47:49 +0700

    chore(deps): upgrade ESLint packages and migrate shared configs to TypeScript

--- packages/eslint-config/next.ts
+++ packages/eslint-config/next.ts
@@ -6,7 +6,7 @@ import pluginReactHooks from "eslint-plugin-react-hooks";
 import pluginReact from "eslint-plugin-react";
 import globals from "globals";
 import pluginNext from "@next/eslint-plugin-next";
-import { config as baseConfig } from "./base.js";
+import { config as baseConfig } from "./base";
 
 /**
  * A custom ESLint configuration for libraries that use Next.js.
--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -1,4 +1,13 @@
 import { config } from "@workspace/eslint-config/react-internal";
 
 /** @type {import("eslint").Linter.Config} */
-export default config;
+export default [
+	...config,
+	{
+		settings: {
+			react: {
+				version: "19",
+			},
+		},
+	},
+];
